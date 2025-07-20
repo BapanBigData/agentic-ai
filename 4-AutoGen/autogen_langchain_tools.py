51838b66adcedf6b1ef5bd740536f1951a4651ac
BapanBigData <bapanmldl7892@gmail.com>
2025-07-21 00:34:42 +0530

    pushed

---
+++ 4-AutoGen/autogen_langchain_tools.py
@@ -0,0 +1,66 @@
+import os
+import certifi
+os.environ["SSL_CERT_FILE"] = certifi.where()
+
+import asyncio
+from autogen_agentchat.agents import AssistantAgent
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from langchain_tavily import TavilySearch
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
+
+if not api_key:
+    raise ValueError("Please set the OPENAI_API_KEY environment variable.")
+
+
+model_client=OpenAIChatCompletionClient(
+    model='gpt-4o-mini',
+    temperature=0.3,
+    api_key=api_key
+)
+
+search_tool = TavilySearch()
+
+def search_web(query:str) ->str:
+    """Search the web for the given query and return the results."""
+    
+    try:
+        results = search_tool.invoke(query)
+        return results
+    except Exception as e:
+        print(f"Error occurred while searching the web: {e}")
+        return "No results found." 
+    
+
+search_agent = AssistantAgent(
+    name="SearchAgent",
+    model_client=model_client,
+    tools=[search_web],
+    description="An agent that can search the web for information.",
+    system_message="You are a helpful assistant that can search the web for information using the search_web tool." \
+    "Please make sure that you use the search_web tool to find information before you return the answer." \
+    "don't send the year in query, rather use latest or recently etc.",
+    reflect_on_tool_use=True,
+)
+
+async def run_search():
+    """Run the search agent with a sample query."""
+    
+    query = "What is the latest news on crude oil and Trump Tariff?" 
+    print(f"Querying: {query}")
+    
+    
+    result = await search_agent.run(task=query)
+    
+    print(result.messages[-1])
+    print('\n')
+    
+    print(result.messages[-1].content)
+
+
+if __name__ == "__main__":
+    asyncio.run(run_search())
---
+++ 4-AutoGen/in_built_tools.py
@@ -0,0 +1,90 @@
+import os
+import certifi
+os.environ["SSL_CERT_FILE"] = certifi.where()
+
+import asyncio
+from autogen_agentchat.agents import AssistantAgent
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_ext.tools.http import HttpTool
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("Please set the OPENAI_API_KEY environment variable.")
+
+
+model_client=OpenAIChatCompletionClient(
+    model='gpt-4o-mini',
+    temperature=0.3,
+    api_key=api_key
+)
+
+
+'''
+{
+    "fact": "Cats with long, lean bodies are more likely to be outgoing, and more protective and vocal than those with a stocky build.",
+    "length": 121
+}
+'''
+
+"""
+{
+    'fact': 'Foods that should not be given to cats include onions, garlic, green tomatoes, raw potatoes, chocolate, grapes, and raisins. Though milk is not toxic, it can cause an upset stomach and gas. Tylenol and aspirin are extremely toxic to cats, as are many common houseplants. Feeding cats dog food or canned tuna that’s for human consumption can cause malnutrition.', 
+    'length': 360}
+"""
+
+"url: https://catfact.ninja/fact"
+
+
+schema = {
+        "type": "object",
+        "properties": {
+            "fact": {
+                "type": "string",
+                "description": "A random cat fact"
+            },
+            "length": {
+                "type": "integer",
+                "description": "Length of the cat fact"
+            }
+        },
+        "required": ["fact", "length"],
+    }
+
+
+http_tool = HttpTool(
+    name="cat_facts_api",
+    description="get a cool cat fact",
+    scheme="https",
+    host="catfact.ninja",
+    port=443,
+    path="/fact",
+    method="GET",
+    return_type="json",
+    json_schema=schema
+)
+
+
+agent = AssistantAgent(
+    name="CatFactsAgent",
+    model_client=model_client,
+    system_message='You are a helpful assistant that can provide cat facts using the cat_facts_api tool. Give the result with summary',
+    tools=[http_tool],
+    reflect_on_tool_use=True
+)
+
+
+
+async def main(): 
+    result = await agent.run(task = 'Give me a random cat fact')
+
+    print(result.messages)
+    print("\n")
+    
+    print(result.messages[-1].content)
+
+if (__name__ == "__main__"):
+    asyncio.run(main())
